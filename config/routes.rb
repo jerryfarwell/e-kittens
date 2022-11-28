f35e24620c187ba1aebc32f9a4fd63a2631b8173
kevin <letourneurkevin8@gmail.com>
2022-11-28 13:19:48 +0100

    prblm_encrypted_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/secret'
-  devise_for :users
+get 'static_pages/secret'
+get 'users/sign_out'
+  devise_for :users 
   root "static_pages#index"
-  
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
     get 'success', to: 'checkout#success', as: 'checkout_success'
     get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
-end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
