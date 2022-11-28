36da4a38cf515131ac66b4584ceaab78d3aa0412
kevin <letourneurkevin8@gmail.com>
2022-11-28 09:50:39 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
+  root "static_pages#index"
+  
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
