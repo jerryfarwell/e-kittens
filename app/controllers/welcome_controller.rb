e2f30f3d70f7f29fb4f3e6cbd917394940911a32
JERRY farwell <114482666+jerryfarwell@users.noreply.github.com>
2022-12-02 00:04:46 +0100

    front_end added to back_end

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,24 @@
 class WelcomeController < ApplicationController
   def index
+@welcome = Welcome.all
   end
 
   def show
   end
 
   def new
+    @welcome = Welcome.new
   end
 
   def edit
+    @welcome = Welcome.new
+  end
+
+  def create
+    @welcome = Welcome.create(name: params[:name])
+
+    if @welcome.save
+      redirect_to welcome_path
+    end
   end
 end
